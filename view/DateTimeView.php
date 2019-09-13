29ceea7b77ba41f93174efa06928c557842aca25
Thimmy Stenlund <ts222ub@student.lnu.se>
2019-09-13 08:33:20 +0200

    Datetime seems fixed.

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,15 @@ class DateTimeView {
 
 
 	public function show() {
-    date_default_timezone_set('Europe/Stockholm');
-		$timeString = date('d/m/Y == H:i:s');
+    // date_default_timezone_set('Europe/Stockholm');
+    $dayName = date('l');
+    $dayNum = date('d');
+    $dayEnding = date('S');
+    $month = date('F');
+    $year = date('Y');
+    $time = date('H:i:s');
+
+		$timeString = $dayName . ", the " . $dayNum . $dayEnding . " of " . $month . " " . $year . ", The time is " . $time;
 
 		return '<p>' . $timeString . '</p>';
 	}
